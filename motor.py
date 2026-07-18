be6124c5396ec4bcdd81cd184a3706974c4ac689
laza-ro <lazaro.piola@hotmail.com>
2026-07-18 16:53:05 -0300

    feat: ajuste na conversão de ms para segundos, variável PAUSE zerada para não travar o programa.

--- motor.py
+++ motor.py
@@ -1,33 +1,36 @@
-# Criar funções de exemplo para o autoclicker.TimeoutError
+# Criar funções de exemplo para o autoclicker.
 import time
 import pyautogui
+import keyboard
+
+pyautogui.PAUSE = 0
 
 class MotorClicker:
     #método construtor
-    def __init__(self, intervalo_ms, tecla_parar, qtd_cliques, pos_x, pos_y):
-        self.intervalo_ms = intervalo_ms
+    def __init__(self, intervalo_ms, tecla_parar, pos_x, pos_y):
+        self.intervalo_segundos = intervalo_ms / 1000 #conversao de ms para segundos
         self.tecla_parar = tecla_parar
-        self.qtd_cliques = qtd_cliques
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.rodando = False
 
-    def clique(self):
+    def iniciar(self):
         print('Iniciando Autoclicker em 3 segundos...')
         time.sleep(3)
         self.rodando = True
+        keyboard.add_hotkey(self.tecla_parar, self.parar)
         while self.rodando:
             pyautogui.click(self.pos_x, self.pos_y)
-            time.sleep(self.intervalo_ms)
+            time.sleep(self.intervalo_segundos)
     
     def parar(self):
-        print('parando cliques')
-    
+        self.rodando = False
+        print ('Autoclicker parado!')
+
     def ExibirInformacoesAtuais (self):
-        print(self.intervalo_ms)
+        print(self.intervalo_segundos)
         print(self.tecla_parar)
-        print(self.qtd_cliques)
         print(self.pos_x)
         print(self.pos_y)
-    pass
+
 
