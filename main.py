454b1baec260a5461629cd0ec978b3a1b20a452a
laza-ro <lazaro.piola@hotmail.com>
2026-07-18 16:33:22 -0300

    feat: classe MotorClicker e função clique para rodar o programa.

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import pyautogui
-import time
+from interface import;
+from motor import MotorClicker;
 
-print('Será iniciado em 5 segundos...')
-time.sleep(5)
 
-for i in range(100):
-    pyautogui.click()
-    time.sleep(2)  # segundos
+
--- motor.py
+++ motor.py
@@ -1,10 +1,33 @@
 # Criar funções de exemplo para o autoclicker.TimeoutError
 import time
+import pyautogui
 
+class MotorClicker:
+    #método construtor
+    def __init__(self, intervalo_ms, tecla_parar, qtd_cliques, pos_x, pos_y):
+        self.intervalo_ms = intervalo_ms
+        self.tecla_parar = tecla_parar
+        self.qtd_cliques = qtd_cliques
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.rodando = False
 
+    def clique(self):
+        print('Iniciando Autoclicker em 3 segundos...')
+        time.sleep(3)
+        self.rodando = True
+        while self.rodando:
+            pyautogui.click(self.pos_x, self.pos_y)
+            time.sleep(self.intervalo_ms)
+    
+    def parar(self):
+        print('parando cliques')
+    
+    def ExibirInformacoesAtuais (self):
+        print(self.intervalo_ms)
+        print(self.tecla_parar)
+        print(self.qtd_cliques)
+        print(self.pos_x)
+        print(self.pos_y)
+    pass
 
-def iniciar_cliques():
-    print('Iniciando')
-    time.sleep(3)
-
-def parar_cliques()
