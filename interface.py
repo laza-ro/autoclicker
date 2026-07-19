a0bea39bc58c7dbf01132d2134690fe84138796f
laza-ro <lazaro.piola@hotmail.com>
2026-07-18 23:03:18 -0300

    fix: duplicidade de telas criadas na execução.

--- interface.py
+++ interface.py
@@ -5,46 +5,46 @@ from motor import MotorClicker
 import threading
 
 class AppInterface:
-    def __init__(self): #criando a tela principal do projeto
-        self.tela = tk.Tk()
-        self.tela.title ("Auto Clicker")
-        self.tela.geometry("400x400")
-        self.tela.resizable(True, True)
+    def __init__(self, root): #criando a tela principal do projeto
+        self.root = root
+        self.root.title ("Auto Clicker")
+        self.root.geometry("400x400")
+        self.root.resizable(True, True)
         
 
     #desenho do projeto
         #espaço pra inserir os valores de ms
-        self.label_ms = tk.Label(self.tela, text="Intervalo em ms")
+        self.label_ms = tk.Label(self.root, text="Intervalo em ms")
         self.label_ms.pack(pady = 5) #espaço nas bordas
-        self.entrada_ms = tk.Entry(self.tela)
+        self.entrada_ms = tk.Entry(self.root)
         self.entrada_ms.pack()
         #espaço pra inserir a posição X na tela
-        self.label_pos_x = tk.Label(self.tela, text = "Posição X")
+        self.label_pos_x = tk.Label(self.root, text = "Posição X")
         self.label_pos_x.pack(pady = 5) #espaço nas bordas
-        self.entrada_pos_x = tk.Entry(self.tela)
+        self.entrada_pos_x = tk.Entry(self.root)
         self.entrada_pos_x.pack()
         #espaço pra inserir a posição Y na tela
-        self.label_pos_y = tk.Label(self.tela, text = "Posição Y")
+        self.label_pos_y = tk.Label(self.root, text = "Posição Y")
         self.label_pos_y.pack(pady = 5) #espaço nas bordas
-        self.entrada_pos_y = tk.Entry(self.tela)
+        self.entrada_pos_y = tk.Entry(self.root)
         self.entrada_pos_y.pack()
         #espaço pra hotkey
-        self.label_tecla_parar = tk.Label(self.tela, text = "Insira a tecla de atalho para parar")
+        self.label_tecla_parar = tk.Label(self.root, text = "Insira a tecla de atalho para parar")
         self.label_tecla_parar.pack(pady = 5)
-        self.entrada_tecla_parar = tk.Entry(self.tela)
+        self.entrada_tecla_parar = tk.Entry(self.root)
         self.entrada_tecla_parar.pack()
         #criação dos botões
         #botão iniciar
-        self.btn_iniciar = tk.Button (self.tela, text = "Iniciar", command=self.iniciar_clicker)
+        self.btn_iniciar = tk.Button (self.root, text = "Iniciar", command=self.iniciar_clicker)
         self.btn_iniciar.pack(pady = 10)
         #botão parar
-        self.btn_parar = tk.Button (self.tela, text = "Parar", command = self.parar_clicker)
+        self.btn_parar = tk.Button (self.root, text = "Parar", command = self.parar_clicker)
         self.btn_parar.pack(pady=10)
         #capturar posição (tela)
-        self.btn_capturar_posicao = tk.Button (self.tela, text = "Capturar posição", command = self.capturar_posicao)
+        self.btn_capturar_posicao = tk.Button (self.root, text = "Capturar posição", command = self.capturar_posicao)
         self.btn_capturar_posicao.pack(pady = 10)
 
-        self.tela.mainloop()
+        self.root.mainloop()
 
     #funções dos botões criados anteriormente
     def iniciar_clicker(self):
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import tkinter as tk
 from interface import AppInterface
 
-root = tk.Tk () #cria a janela do sistema 
+root = tk.Tk() #cria a janela do sistema 
 app = AppInterface(root) # cria a interface gráfica do interface.py
 
-root.mainloop () #mantém a janela aberta
-
-
+root.mainloop() #mantém a janela aberta
